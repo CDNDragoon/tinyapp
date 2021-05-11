ba3e8f11343c47fca59f82430e2bc6f693364167
CDNDragoon <tjgossage@gmail.com>
2021-05-11 23:49:54 +0000

    edit button

--- express_server.js
+++ express_server.js
@@ -58,7 +58,12 @@ app.post("/urls", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
-})
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL
+  res.redirect(`/urls/${req.params.shortURL}`)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
